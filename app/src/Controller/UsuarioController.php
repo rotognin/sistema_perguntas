f63e33ca46899931c7792ec1fa680a2ab4dc9f32
Rodrigo Tognin <rotog@outlook.com>
2022-02-12 16:10:24 -0300

    Implementando o registro do usuário

--- app/src/Controller/UsuarioController.php
+++ app/src/Controller/UsuarioController.php
@@ -11,4 +11,30 @@ class UsuarioController extends Controller
         criarCsrf();
         parent::view('usuario.cadastro');
     }
+
+    public static function gravar(array $post, array $get)
+    {
+        if (!isset($post['_token']) || $post['_token'] != $_SESSION['csrf']){
+            parent::logout($post, $get, 'Acesso incorreto. Realize um novo login.');
+            exit;
+        }
+
+        // Fazer as validações
+
+        // Ver se as duas senhas são iguais
+
+
+        // Colocar como status = 3 (pendente de ativação)
+
+
+        // Gravar o registro
+
+
+        // Enviar um código por e-mail
+
+        // Encaminhar o usuário para uma página onde ele deverá digitar o código enviado
+        // por e-mail
+
+        
+    }
 }
