6a7c633419a8068331528936b282fd1d57a520a4
Rodrigo Tognin <rotog@outlook.com>
2022-02-14 10:57:17 -0300

    Adição dos arquivos de configuração do projeto

---
+++ app/src/config.php
@@ -0,0 +1,23 @@
+<?php
+
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "perguntas",
+    "username" => "root",
+    "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
+
+define("EMAIL_DATA",[
+    "host" => "",
+    "user" => "",
+    "pass" => "",
+    "port" => 465
+]);
