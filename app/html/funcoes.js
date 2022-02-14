981376240b49f630cb181d173c448bd2aae141eb
Rodrigo Tognin <rotog@outlook.com>
2022-02-14 14:50:31 -0300

    Implementando o sistema de votação nas respostas

--- app/html/funcoes.js
+++ app/html/funcoes.js
@@ -1,4 +1,6 @@
 <script>
+    // Fazer a parte do Ajax, para gravar a votação do usuário
+
     function VotarPositivo(resposta_id){
         console.log("Votou positivo na resposta " + resposta_id)
     }
